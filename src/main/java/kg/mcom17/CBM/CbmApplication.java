b4eaa8093696c8e3219a9774604a6ac902e6fc4e
Zhavlon <38985489+java93@users.noreply.github.com>
2018-12-04 16:37:48 +0000

    Start of the project
    Created project from https://start.spring.io/

---
+++ src/main/java/kg/mcom17/CBM/CbmApplication.java
@@ -0,0 +1,12 @@
+package kg.mcom17.CBM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbmApplication.class, args);
+	}
+}
---
+++ src/test/java/kg/mcom17/CBM/CbmApplicationTests.java
@@ -0,0 +1,16 @@
+package kg.mcom17.CBM;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CbmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
